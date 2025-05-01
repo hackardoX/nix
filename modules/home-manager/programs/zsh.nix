fb5089ddcd487d67889efe149ffa4f1518f29831
andrea11 <10788630+andrea11@users.noreply.github.com>
2025-05-01 23:12:21 +1200

    update deprecated zsh

--- modules/home-manager/programs/zsh.nix
+++ modules/home-manager/programs/zsh.nix
@@ -10,7 +10,7 @@ in
       EDITOR = "code --wait";
       VISUAL = "code --wait";
     };
-    initExtra =
+    initContent =
       ''
         if [[ -f /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh ]]; then
           . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
