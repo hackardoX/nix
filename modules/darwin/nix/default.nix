b46bf693429d8a8ca71c936b901e6a62f65d10d5
hackardoX <hackardo@gmail.com>
2025-11-17 19:09:45 +1300

    ci: add build in ci
    Signed-off-by: hackardoX <hackardo@gmail.com>

--- modules/darwin/nix/default.nix
+++ modules/darwin/nix/default.nix
@@ -155,11 +155,12 @@ in
             "https://nix-community.cachix.org"
             "https://nixpkgs-unfree.cachix.org"
             "https://numtide.cachix.org"
+            "https://hackardo.cachix.org"
           ];
 
           trusted-public-keys = [
             "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-            "aaccardo.cachix.org-1:lonn0TzLICqhzw+srQyQsMQ4HRgrTuw2ckAQOsKDajs="
+            "hackardo.cachix.org-1:sQXwmhqZP1B1qMogZFGbm3FDagEiwNbG4zgi80Elda0="
             "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
             "nixpkgs-unfree.cachix.org-1:hqvoInulhbV4nJ9yJOEr+4wxhDV4xq2d1DK7S6Nj6rs="
             "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
