f83b956ddb31d54192095902a2d3452908bab997
hackardoX <hackardo@gmail.com>
2025-12-24 21:01:35 +1300

    feat: replace vscode with neovim
    Signed-off-by: hackardoX <hackardo@gmail.com>

--- modules/home-manager/darwin.nix
+++ modules/home-manager/darwin.nix
@@ -4,30 +4,28 @@
   ...
 }:
 {
-  flake.modules.darwin.base =
-    darwinArgs:
-    {
-      imports = [ inputs.home-manager.darwinModules.home-manager ];
+  flake.modules.darwin.base = darwinArgs: {
+    imports = [ inputs.home-manager.darwinModules.home-manager ];
 
-      home-manager = {
-        useGlobalPkgs = true;
-        useUserPackages = true;
-        backupFileExtension = "hm.old";
-        verbose = true;
+    home-manager = {
+      useGlobalPkgs = true;
+      useUserPackages = true;
+      backupFileExtension = "hm.old";
+      verbose = true;
 
-        users.${darwinArgs.config.system.primaryUser}.imports = [
-          {
-            # This value determines the NixOS release from which the default
-            # settings for stateful data, like file locations and database versions
-            # on your system were taken. It‘s perfectly fine and recommended to leave
-            # this value at the release version of the first install of this system.
-            # Before changing this value read the documentation for this option
-            # (e.g. man configuration.nix or on https://search.nixos.org/options?&show=system.stateVersion&from=0&size=50&sort=relevance&type=packages&query=stateVersion).
-            # TODO: Replace with variable
-            home.stateVersion = "24.11";
-          }
-          config.flake.modules.homeManager.base
-        ];
-      };
+      users.${darwinArgs.config.system.primaryUser}.imports = [
+        {
+          # This value determines the NixOS release from which the default
+          # settings for stateful data, like file locations and database versions
+          # on your system were taken. It‘s perfectly fine and recommended to leave
+          # this value at the release version of the first install of this system.
+          # Before changing this value read the documentation for this option
+          # (e.g. man configuration.nix or on https://search.nixos.org/options?&show=system.stateVersion&from=0&size=50&sort=relevance&type=packages&query=stateVersion).
+          # TODO: Replace with variable
+          home.stateVersion = "24.11";
+        }
+        config.flake.modules.homeManager.base
+      ];
     };
+  };
 }
