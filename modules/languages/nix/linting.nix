1e4a83b417bf0e1f76b07089b453277fb7b61303
aaccardo <hackardo@gmail.com>
2026-01-02 00:21:31 +1300

    feat: fix nix linting
    Signed-off-by: aaccardo <hackardo@gmail.com>

--- modules/languages/nix/linting.nix
+++ modules/languages/nix/linting.nix
@@ -1,14 +1,11 @@
 {
-  perSystem =
-    { inputs', ... }:
-    {
-      treefmt.programs = {
-        nixf-diagnose.enable = true;
+  perSystem = {
+    treefmt.programs = {
+      nixf-diagnose.enable = true;
 
-        statix = {
-          enable = true;
-          package = inputs'.statix.packages.default;
-        };
+      statix = {
+        enable = true;
       };
     };
+  };
 }
