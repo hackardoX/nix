85d9db27f1ae1a5e8e6aacba7853da638ddf3c92
aaccardo <hackardo@gmail.com>
2026-01-02 12:20:00 +1300

    feat: add variable for home stateVersion
    Signed-off-by: aaccardo <hackardo@gmail.com>

--- modules/hosts/Andrea-MacBook-Air/state-version.nix
+++ modules/hosts/Andrea-MacBook-Air/state-version.nix
@@ -1,5 +1,8 @@
 {
-  configurations.darwin.Andrea-MacBook-Air.module = {
-    system.stateVersion = 5;
-  };
+  configurations.darwin.Andrea-MacBook-Air.module =
+    { config, ... }:
+    {
+      system.stateVersion = 5;
+      home-manager.users.${config.system.primaryUser}.home.stateVersion = "24.11";
+    };
 }
