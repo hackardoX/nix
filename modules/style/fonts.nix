7c6b25e36d4c4b0f90415710415e026dbb4fb838
aaccardo <hackardo@gmail.com>
2026-01-02 20:45:31 +1300

    feat: tweak nvim clipboard and font
    Signed-off-by: aaccardo <hackardo@gmail.com>

--- modules/style/fonts.nix
+++ modules/style/fonts.nix
@@ -1,19 +1,37 @@
-let
-  fontPackages = pkgs: [ pkgs.nerd-fonts.monaspace ];
-in
+{ config, lib, ... }:
 {
-  flake.modules = {
+  options.fonts.default = lib.mkOption {
+    type = lib.types.submodule {
+      options = {
+        name = lib.mkOption {
+          type = lib.types.str;
+          default = "meslo-lg";
+          readOnly = true;
+          description = "Font package name in nerd-fonts";
+        };
+        family = lib.mkOption {
+          type = lib.types.str;
+          default = "MesloLGL Nerd Font";
+          readOnly = true;
+          description = "Font family name for applications";
+        };
+      };
+    };
+    default = { };
+  };
+
+  config.flake.modules = {
     darwin.base =
       { pkgs, ... }:
       {
-        fonts.packages = fontPackages pkgs;
+        fonts.packages = [ pkgs.nerd-fonts.${config.fonts.default.name} ];
         system.defaults.NSGlobalDomain.AppleFontSmoothing = 1;
       };
 
     homeManager.base =
       { pkgs, ... }:
       {
-        home.packages = fontPackages pkgs;
+        home.packages = [ pkgs.nerd-fonts.${config.fonts.default.name} ];
       };
   };
 }
