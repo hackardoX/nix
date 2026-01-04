f1a6f2691e80d7e3952f5ee9af6390f592083829
aaccardo <hackardo@gmail.com>
2026-01-04 19:16:39 +1300

    fix: tweak yazi in neovim
    Signed-off-by: aaccardo <hackardo@gmail.com>

--- modules/nixvim/file-tree.nix
+++ modules/nixvim/file-tree.nix
@@ -1,3 +1,43 @@
 {
-  flake.modules.nixvim.base.plugins.yazi.enable = true;
+  flake.modules.nixvim.base = {
+    plugins.yazi = {
+      enable = true;
+      settings = {
+        open_for_directories = false;
+        keymaps = {
+          show_help = "<f1>";
+        };
+      };
+    };
+
+    keymaps = [
+      {
+        mode = [
+          "n"
+          "v"
+        ];
+        key = "<leader>-";
+        action = "<cmd>Yazi<cr>";
+        options = {
+          desc = "Open yazi at the current file";
+        };
+      }
+      {
+        mode = "n";
+        key = "<leader>cw";
+        action = "<cmd>Yazi cwd<cr>";
+        options = {
+          desc = "Open the file manager in nvim's working directory";
+        };
+      }
+      {
+        mode = "n";
+        key = "<c-up>";
+        action = "<cmd>Yazi toggle<cr>";
+        options = {
+          desc = "Resume the last yazi session";
+        };
+      }
+    ];
+  };
 }
