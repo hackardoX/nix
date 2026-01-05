01852482fc471039056c025b3a842854ad1cec3f
aaccardo <hackardo@gmail.com>
2026-01-05 20:42:40 +1300

    feat: enhance js formatting
    Signed-off-by: aaccardo <hackardo@gmail.com>

--- modules/languages/javascript.nix
+++ modules/languages/javascript.nix
@@ -2,5 +2,25 @@
   flake.modules.nixvim.base.plugins = {
     lsp.servers.eslint.enable = true;
     typescript-tools.enable = true;
+    perSystem.treefmt.programs.biome = {
+      enable = true;
+      settings = {
+        formatter.formatWithErrors = true;
+        assist = {
+          actions.source.useSortedAttributes = "on";
+        };
+        css = {
+          formatter.enabled = true;
+          parser.cssModules = true;
+        };
+        linter = {
+          rules.nursery.useSortedClasses = {
+            level = "error";
+            fix = "safe";
+            options = { };
+          };
+        };
+      };
+    };
   };
 }
