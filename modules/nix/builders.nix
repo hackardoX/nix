851a2af40e995ceb7b9bbb4446295b97026bd6e9
aaccardo <hackardo@gmail.com>
2026-02-28 18:40:19 +1300

    build: update flake
    Signed-off-by: aaccardo <hackardo@gmail.com>

--- modules/nix/builders.nix
+++ modules/nix/builders.nix
@@ -1,26 +1,34 @@
-{ self, ... }:
+{ inputs, ... }:
+let
+  baseLinuxBuilder = {
+    enable = true;
+    ephemeral = true;
+    systems = [
+      "x86_64-linux"
+      "aarch64-linux"
+    ];
+    maxJobs = 4;
+    config = {
+      virtualisation = {
+        darwin-builder = {
+          diskSize = 20 * 1024;
+          memorySize = 8 * 1024;
+        };
+        cores = 6;
+      };
+    };
+  };
+in
 {
-  flake.packages.aarch64-linux.linux-builder =
-    self.darwinConfigurations.Andrea-MacBook-Air.config.nix.linux-builder.config.system.build.toplevel;
+  flake.packages.aarch64-darwin.linux-builder =
+    (inputs.darwin.lib.darwinSystem {
+      modules = [ { nix.linux-builder = baseLinuxBuilder; } ];
+    }).config.nix.linux-builder.package;
 
   flake.modules.darwin.laptop = {
     nix = {
-      linux-builder = {
-        enable = true;
-        ephemeral = true;
-        systems = [
-          "x86_64-linux"
-          "aarch64-linux"
-        ];
-        maxJobs = 4;
-        config = {
-          virtualisation = {
-            darwin-builder = {
-              diskSize = 20 * 1024;
-              memorySize = 8 * 1024;
-            };
-            cores = 6;
-          };
+      linux-builder = baseLinuxBuilder // {
+        config = baseLinuxBuilder.config // {
           boot.binfmt.emulatedSystems = [ "x86_64-linux" ];
         };
       };
