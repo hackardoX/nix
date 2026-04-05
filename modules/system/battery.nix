5c0bbdd13dc38600bdf0b71236cf2ea9017b8046
Andrea Accardo <hackardo@gmail.com>
2026-04-05 22:09:46 +1200

    feat: remove aldente (#185)

--- modules/system/battery.nix
+++ modules/system/battery.nix
@@ -3,20 +3,9 @@
   flake.modules.homeManager.laptop =
     { pkgs, ... }:
     lib.mkIf pkgs.stdenv.hostPlatform.isDarwin {
-      home.packages = [
-        pkgs.aldente
-      ];
-
       targets.darwin.defaults = {
-        "com.apphousekitchen.aldente-pro" = {
-          LaunchAtLogin__hasMigrated = 1;
-          SUEnableAutomaticChecks = 1;
-          SUHasLaunchedBefore = 1;
-          chargeVal = 80;
-          checkForUpdates = 1;
-          launchAtLogin = 1;
-          showDockIcon = 0;
-          showGUIonStartup = 1;
+        "com.apple.batteryui.charging.mac" = {
+          "com.apple.batteryui.charging.mac.prior.limit" = 80;
         };
       };
     };
