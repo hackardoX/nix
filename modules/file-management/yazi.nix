193cacdf7417fc84ec2d48304dd6cc775f5bbb49
aaccardo <hackardo@gmail.com>
2026-05-12 19:33:58 +1000

    feat(yazi): enhance previewers
    Signed-off-by: aaccardo <hackardo@gmail.com>

--- modules/file-management/yazi.nix
+++ modules/file-management/yazi.nix
@@ -11,11 +11,43 @@
           require("full-border"):setup()
         '';
 
-        plugins = {
-          inherit (pkgs.yaziPlugins) full-border piper;
+        plugins =
+          let
+            mux = pkgs.stdenvNoCC.mkDerivation {
+              pname = "mux.yazi";
+              version = "unstable";
+              src = pkgs.fetchFromGitHub {
+                owner = "peterfication";
+                repo = "mux.yazi";
+                rev = "main";
+                hash = "sha256-Cf/gtv3uIwXtkp6pEZJSkylA3vHpmQqrWOLo2FLg9yA=";
+              };
+              installPhase = ''
+                cp -r $src $out
+              '';
+            };
+          in
+          {
+            inherit (pkgs.yaziPlugins) full-border piper;
+            inherit mux;
+          };
+
+        keymap = {
+          mgr.prepend_keymap = [
+            {
+              on = [ "P" ];
+              run = "plugin mux next";
+              desc = "Cycle previewer";
+            }
+          ];
         };
 
         settings = {
+          mgr.ratio = [
+            1
+            2
+            5
+          ];
           opener = {
             open = [
               {
