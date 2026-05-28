edee2c26ca568781c176af74ebc9f0d59cdd0b22
aaccardo <hackardo@gmail.com>
2026-05-28 19:25:07 +0900

    refactor: security folder
    Signed-off-by: aaccardo <hackardo@gmail.com>

---
+++ modules/security/fail2ban.nix
@@ -0,0 +1,53 @@
+{
+  flake.modules.nixos.homelab = nixosArgs: {
+    programs.msmtp = {
+      enable = true;
+      setSendmail = true;
+      defaults = {
+        port = 587;
+        auth = "plain";
+        tls = "on";
+        tls_starttls = "on";
+        account = "fail2ban";
+      };
+      accounts.fail2ban = {
+        host = "smtp.resend.com";
+        user = "resend";
+        passwordeval = "cat ${nixosArgs.config.services.onepassword-secrets.secretPaths.resendApiKey}";
+        from = "fail2ban@YOUR_RESEND_DOMAIN";
+      };
+    };
+
+    services = {
+      fail2ban = {
+        enable = true;
+        maxretry = 3;
+        bantime = "24h";
+        jails = {
+          ssh-iptables.settings = {
+            enabled = true;
+            port = "ssh";
+            filter = "sshd";
+            logpath = "/var/log/auth.log";
+            maxretry = 3;
+            bantime = "24h";
+            destemail = nixosArgs.config.flake.meta.users.hal.email;
+            action = ''
+              %(action_mwl)s
+            '';
+          };
+        };
+      };
+
+      onepassword-secrets.secrets = {
+        resendApiKey = {
+          path = "/run/secrets/resend_api_key";
+          reference = "op://Development/Resend/api key";
+          owner = "fail2ban";
+          group = "fail2ban";
+          services = [ "fail2ban" ];
+        };
+      };
+    };
+  };
+}
