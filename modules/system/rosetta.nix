4c4ce581277313daae62394f69aa71ca8ebfe8d6
aaccardo <hackardo@gmail.com>
2026-06-01 13:40:18 +0900

    fix: typo modules
    Signed-off-by: aaccardo <hackardo@gmail.com>

--- modules/system/rosetta.nix
+++ modules/system/rosetta.nix
@@ -1,5 +1,5 @@
 {
-  flake.module.darwin.base = {
+  flake.modules.darwin.base = {
     system.activationScripts.postActivation.text = /* Bash */ ''
       echo "Installing Rosetta..."
       softwareupdate --install-rosetta --agree-to-license
