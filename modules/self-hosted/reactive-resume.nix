7003b70b8cdf0cb13b300408011cbbf0b5a99a1a
aaccardo <hackardo@gmail.com>
2026-06-09 16:23:48 +0200

    feat: add rxresume service
    Signed-off-by: aaccardo <hackardo@gmail.com>

---
+++ modules/self-hosted/reactive-resume.nix
@@ -0,0 +1,45 @@
+{ config, ... }:
+let
+  domain = config.flake.meta.reverse-proxy.domain;
+  port = config.flake.meta.reverse-proxy.ports.reactive-resume;
+  appSubDomain = "rxresume";
+in
+{
+  flake.modules.nixos.homelab = {
+    services.caddy.virtualHosts."${appSubDomain}.${domain}" = {
+      useACMEHost = domain;
+      extraConfig = ''
+        import reverse_proxy_common
+        reverse_proxy localhost:${toString port}
+      '';
+    };
+  };
+
+  flake.modules.homeManager."${config.flake.meta.reactive-resume.user}@homelab" = hmArgs: {
+    services.reactive-resume = {
+      enable = true;
+      port = port;
+      appUrl = "https://${appSubDomain}.${domain}";
+      authSecretFile =
+        hmArgs.config.services.onepassword-secrets.secretPaths.reactiveResumeAuthSecretPath;
+      database.passwordFile =
+        hmArgs.config.services.onepassword-secrets.secretPaths.reactiveResumeDbPasswordPath;
+    };
+
+    programs.onepassword-secrets.secrets = {
+      reactiveResumeAuthSecretPath = {
+        path = "/run/secrets/reactive-resume/auth_secret";
+        reference = "op://Homelab/Reactive Resume/Auth/secret";
+        owner = config.flake.meta.reactive-resume.user;
+        group = config.flake.meta.reactive-resume.group;
+      };
+
+      reactiveResumeDbPasswordPath = {
+        path = "/run/secrets/reactive-resume/db_password";
+        reference = "op://Homelab/Reactive Resume/Database/password";
+        owner = config.flake.meta.reactive-resume.user;
+        group = config.flake.meta.reactive-resume.group;
+      };
+    };
+  };
+}
