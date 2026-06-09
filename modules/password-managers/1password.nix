d35395edd2675bf208dc6b5085afb10957c201fd
aaccardo <hackardo@gmail.com>
2026-06-09 20:34:25 +0200

    refactor: cleanup args
    Signed-off-by: aaccardo <hackardo@gmail.com>

--- modules/password-managers/1password.nix
+++ modules/password-managers/1password.nix
@@ -14,10 +14,10 @@
   };
 
   flake.modules.homeManager.password-manager =
-    { config, pkgs, ... }:
+    hmArgs@{ pkgs, ... }:
     let
-      _1passwordOriginalSocketPath = "${config.home.homeDirectory}/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock";
-      _1passwordSymLinkSocketPath = "${config.xdg.dataHome}/.1password/agent.sock";
+      _1passwordOriginalSocketPath = "${hmArgs.config.home.homeDirectory}/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock";
+      _1passwordSymLinkSocketPath = "${hmArgs.config.xdg.dataHome}/.1password/agent.sock";
     in
     {
       imports = [ inputs.op-shell-plugins.hmModules.default ];
@@ -28,7 +28,7 @@
 
         file = {
           "${_1passwordSymLinkSocketPath}" = {
-            source = config.lib.file.mkOutOfStoreSymlink _1passwordOriginalSocketPath;
+            source = hmArgs.config.lib.file.mkOutOfStoreSymlink _1passwordOriginalSocketPath;
           };
 
           ".config/1Password/ssh/agent.toml".text = ''
