975d6e7e0650e195b6c93495842894f025765138
aaccardo <hackardo@gmail.com>
2026-06-13 10:35:32 +0200

    feat: split ssh host and initrd ssh keys
    Signed-off-by: aaccardo <hackardo@gmail.com>

--- modules/hosts/HomeLab/secrets.nix
+++ modules/hosts/HomeLab/secrets.nix
@@ -3,13 +3,25 @@
     services.onepassword-secrets.secrets = {
       homeLabPrivateKey = {
         path = "/etc/ssh/ssh_host_ed25519_key";
-        reference = "op://Development/HomeLab/private key";
+        reference = "op://HomeLab/Hal/private key";
         group = "wheel";
         services = [ "sshd" ];
       };
       homeLabPublicKey = {
         path = "/etc/ssh/ssh_host_ed25519_key.pub";
-        reference = "op://Development/HomeLab/public key";
+        reference = "op://HomeLab/Hal/public key";
+        group = "wheel";
+        services = [ "sshd" ];
+      };
+      homeLabInitrdPrivateKey = {
+        path = "/etc/secrets/initrd/ssh_host_ed25519_key";
+        reference = "op://HomeLab/Initrd Luks/private key";
+        group = "wheel";
+        services = [ "sshd" ];
+      };
+      homeLabInitrdPublicKey = {
+        path = "/etc/secrets/initrd/ssh_host_ed25519_key.pub";
+        reference = "op://HomeLab/Initrd Luks/private key";
         group = "wheel";
         services = [ "sshd" ];
       };
