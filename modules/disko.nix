48ebfa90ad637c1e2f26650c9b1e3d83fbf93505
aaccardo <hackardo@gmail.com>
2026-06-15 22:51:10 +0200

    fix: 2nd attempt fix disko
    Signed-off-by: aaccardo <hackardo@gmail.com>

--- modules/disko.nix
+++ modules/disko.nix
@@ -1,5 +1,9 @@
-{ inputs, ... }: {
-  flake.modules.nixos.base = {
-    imports = [ inputs.disko.flakeModules.default ];
-  };
+{ self, inputs, ... }: {
+  imports = [
+    inputs.disko.flakeModules.default
+  ];
+
+  flake.diskoConfigurations = builtins.mapAttrs (hostname: hostConfig: {
+    disko.devices = hostConfig.config.disko.devices;
+  }) self.nixosConfigurations;
 }
