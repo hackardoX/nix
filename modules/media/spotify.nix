54c9f865d0e0a3211ffa8465ba70c9182071ee11
aaccardo <hackardo@gmail.com>
2026-06-20 23:01:11 +0200

    feat: prevent spotify auto-update

--- modules/media/spotify.nix
+++ modules/media/spotify.nix
@@ -1,12 +1,12 @@
 { inputs, ... }:
 {
-  flake.modules.darwin.laptop =
+  flake.modules.homeManager.laptop =
     { pkgs, ... }:
     let
       spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.stdenv.system};
     in
     {
-      imports = [ inputs.spicetify-nix.darwinModules.spicetify ];
+      imports = [ inputs.spicetify-nix.homeManagerModules.spicetify ];
       programs.spicetify = {
         enable = true;
         enabledCustomApps = with spicePkgs.apps; [
@@ -19,9 +19,50 @@
           hidePodcasts
           shuffle
           betterGenres
+          spicyLyrics
+        ];
+        enabledSnippets = [
+          "${pkgs.writeText "rounded-now-playing-bar.css" ''
+            :root{ --border-radius-1: 8px; }
+            .Root__now-playing-bar, .Root__now-playing-bar footer {
+              border-radius: var(--border-radius-1) !important;
+            }
+          ''}"
+
+          "${pkgs.writeText "remove-gradient.css" ''
+            .main-entityHeader-background, 
+            .main-entityHeader-background.main-entityHeader-overlay, 
+            .main-entityHeader-backgroundColor {
+              background-color: transparent !important;
+              background-image: none !important;
+            }
+
+            .main-actionBarBackground-background,
+            .playlist-playlist-actionBarBackground-background {
+              background-color: transparent !important;
+              background-image: none !important;
+            }
+
+            .main-home-homeHeader {
+              background-color: transparent !important;
+              background-image: none !important;
+            }
+          ''}"
         ];
         theme = spicePkgs.themes.catppuccin;
         colorScheme = "macchiato";
       };
+
+      home.activation.disableSpotifyUpdates =
+        inputs.home-manager.lib.hm.dag.entryAfter [ "writeBoundary" ]
+          ''
+            SPOTIFY_UPDATE_DIR="$HOME/Library/Application Support/Spotify/PersistentCache/Update"
+
+            if ! /usr/bin/stat -f "%Sf" "$SPOTIFY_UPDATE_DIR" 2> /dev/null | grep -q uchg; then
+              rm -rf "$SPOTIFY_UPDATE_DIR"
+              mkdir -p "$SPOTIFY_UPDATE_DIR"
+              /usr/bin/chflags uchg "$SPOTIFY_UPDATE_DIR"
+            fi
+          '';
     };
 }
