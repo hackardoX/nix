7008ced2a49e73341badff8b2a915995e7948aa9
aaccardo <hackardo@gmail.com>
2026-07-18 19:06:26 +0200

    fix: impermanence folders
    Signed-off-by: aaccardo <hackardo@gmail.com>

--- modules/impermanence.nix
+++ modules/impermanence.nix
@@ -83,20 +83,14 @@
       environment.persistence."/persist" = {
         directories = [
           "/etc/nixos"
+          "/etc/ssh"
+          "/etc/secrets/initrd"
           "/var/lib/iwd"
           "/var/lib/opnix"
         ];
         files = [
           "/etc/machine-id"
           "/etc/opnix-token"
-          "/etc/ssh/ssh_host_ecdsa_key"
-          "/etc/ssh/ssh_host_ecdsa_key.pub"
-          "/etc/ssh/ssh_host_ed25519_key"
-          "/etc/ssh/ssh_host_ed25519_key.pub"
-          "/etc/ssh/ssh_host_rsa_key"
-          "/etc/ssh/ssh_host_rsa_key.pub"
-          "/etc/secrets/initrd/ssh_host_ed25519_key"
-          "/etc/secrets/initrd/ssh_host_ed25519_key.pub"
         ];
       };
 
