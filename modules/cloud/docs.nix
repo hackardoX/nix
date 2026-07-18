742876e5684f842468797865e4821c712251abce
aaccardo <hackardo@gmail.com>
2026-07-18 16:52:19 +0200

    fix: 1password secrets' group
    Signed-off-by: aaccardo <hackardo@gmail.com>

--- modules/cloud/docs.nix
+++ modules/cloud/docs.nix
@@ -1,37 +1,41 @@
 {
-  flake.modules.homeManager.laptop = hmArgs: {
-    programs.onepassword-secrets.secrets = {
-      docsPassword = {
-        path = ".secrets/rclone-sync/Documents/password";
-        reference = "op://Homelab/Cloud Encryption/Docs/password";
+  flake.modules.homeManager.laptop =
+    hmArgs@{ pkgs, ... }:
+    {
+      programs.onepassword-secrets.secrets = {
+        docsPassword = {
+          path = ".secrets/rclone-sync/Documents/password";
+          reference = "op://Homelab/Cloud Encryption/Docs/password";
+          group = if pkgs.stdenv.isDarwin then "staff" else "wheel";
+        };
+        docsSalt = {
+          path = ".secrets/rclone-sync/Documents/salt";
+          reference = "op://Homelab/Cloud Encryption/Docs/salt";
+          group = if pkgs.stdenv.isDarwin then "staff" else "wheel";
+        };
       };
-      docsSalt = {
-        path = ".secrets/rclone-sync/Documents/salt";
-        reference = "op://Homelab/Cloud Encryption/Docs/salt";
-      };
-    };
 
-    services.rclone-sync.jobs = {
-      docs-koofr = {
-        localPath = "${hmArgs.config.home.homeDirectory}/Private Docs";
-        destination = "Private Docs";
-        providers = [
-          "koofr"
-        ];
-        encrypted = true;
-        salt = true;
-        passwordFile = hmArgs.config.programs.onepassword-secrets.secretPaths.docsPassword;
-        saltFile = hmArgs.config.programs.onepassword-secrets.secretPaths.docsSalt;
-        schedule = "minutely";
-      };
-      docs-gdrive = {
-        localPath = "${hmArgs.config.home.homeDirectory}/Private Docs";
-        destination = "Private Docs";
-        providers = [
-          "gdrive"
-        ];
-        schedule = "minutely";
+      services.rclone-sync.jobs = {
+        docs-koofr = {
+          localPath = "${hmArgs.config.home.homeDirectory}/Private Docs";
+          destination = "Private Docs";
+          providers = [
+            "koofr"
+          ];
+          encrypted = true;
+          salt = true;
+          passwordFile = hmArgs.config.programs.onepassword-secrets.secretPaths.docsPassword;
+          saltFile = hmArgs.config.programs.onepassword-secrets.secretPaths.docsSalt;
+          schedule = "minutely";
+        };
+        docs-gdrive = {
+          localPath = "${hmArgs.config.home.homeDirectory}/Private Docs";
+          destination = "Private Docs";
+          providers = [
+            "gdrive"
+          ];
+          schedule = "minutely";
+        };
       };
     };
-  };
 }
