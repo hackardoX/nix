9723954d28c66775c3381030fb97ab7d3dfc61f0
aaccardo <hackardo@gmail.com>
2026-07-19 18:14:10 +0200

    fix: impermanence initrd files
    Signed-off-by: aaccardo <hackardo@gmail.com>

--- modules/impermanence.nix
+++ modules/impermanence.nix
@@ -42,8 +42,8 @@
             "/etc/ssh/ssh_host_ed25519_key.pub"
             "/etc/ssh/ssh_host_rsa_key"
             "/etc/ssh/ssh_host_rsa_key.pub"
-            "/etc/secrets/initrd/ssh_host_ed25519_key"
-            "/etc/secrets/initrd/ssh_host_ed25519_key.pub"
+            # "/etc/secrets/initrd/ssh_host_ed25519_key"
+            # "/etc/secrets/initrd/ssh_host_ed25519_key.pub"
           ];
         };
 
