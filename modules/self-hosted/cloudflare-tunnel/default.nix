e0776e2814853560991ed0bbc10b35b5a41ef58a
aaccardo <hackardo@gmail.com>
2026-07-20 16:02:01 +0200

    fix: cloudflare tunnel
    Signed-off-by: aaccardo <hackardo@gmail.com>

--- modules/self-hosted/cloudflare-tunnel/default.nix
+++ modules/self-hosted/cloudflare-tunnel/default.nix
@@ -1,73 +1,73 @@
 {
-  config,
   lib,
-  pkgs,
   ...
 }:
-let
-  cloudflaredStartScript = pkgs.writeShellScript "start-cloudflared" ''
-    exec ${lib.getExe pkgs.cloudflared} tunnel --no-autoupdate run \
-      --token-file "$CREDENTIALS_DIRECTORY"/token
-  '';
-in
 {
-  flake.modules.nixos.homelab = {
-    services = {
-      onepassword-secrets.secrets.cloudflareTunnelTokenFile = {
-        path = "/run/secrets/cloudflared/token";
-        reference = "op://HomeLab/Cloudflare/homelab4.fun/tunnel token";
-        mode = "0400";
-      };
+  flake.modules.nixos.homelab =
+    nixosArgs@{ pkgs, ... }:
+    let
+      cloudflaredStartScript = pkgs.writeShellScript "start-cloudflared" ''
+        exec ${lib.getExe pkgs.cloudflared} tunnel --no-autoupdate run \
+          --token-file "$CREDENTIALS_DIRECTORY"/token
+      '';
+    in
+    {
+      services = {
+        onepassword-secrets.secrets.cloudflareTunnelTokenFile = {
+          path = "/run/secrets/cloudflared/token";
+          reference = "op://HomeLab/Cloudflare/homelab4.fun/tunnel token";
+          mode = "0400";
+        };
 
-      caddy = {
-        globalConfig = lib.mkAfter ''
-          servers {
-            trusted_proxies cloudflare
-            trusted_proxies_strict
-            client_ip_headers Cf-Connecting-Ip X-Forwarded-For
-          }
-        '';
-
-        extraConfig = lib.mkBefore ''
-          (reverse_proxy_common) {
-            import common_headers
-            import geoblock
-            import rate_limit_common
-            import tls_hardened
-            import auth_protected
-
-            request_body {
-              max_size 10MB
+        caddy = {
+          globalConfig = lib.mkAfter ''
+            servers {
+              trusted_proxies cloudflare
+              trusted_proxies_strict
+              client_ip_headers Cf-Connecting-Ip X-Forwarded-For
             }
+          '';
 
-            encode
-          }
-        '';
+          extraConfig = lib.mkBefore ''
+            (reverse_proxy_common) {
+              import common_headers
+              import geoblock
+              import rate_limit_common
+              import tls_hardened
+              import auth_protected
+
+              request_body {
+                max_size 10MB
+              }
+
+              encode
+            }
+          '';
+        };
+      };
+
+      # TODO: Revisit when nixpkgs merges token-based tunnel support
+      # https://github.com/NixOS/nixpkgs/pull/427964
+      # At that point services.cloudflared.tunnels may replace this custom service.
+      systemd.services.cloudflared = {
+        wantedBy = [ "multi-user.target" ];
+        after = [
+          "network-online.target"
+          "opnix-secrets.service"
+        ];
+        wants = [ "network-online.target" ];
+        requires = [ "opnix-secrets.service" ];
+        serviceConfig = {
+          ExecStart = "${cloudflaredStartScript}";
+          LoadCredential = "token:${nixosArgs.config.services.onepassword-secrets.secretPaths.cloudflareTunnelTokenFile}";
+          NoNewPrivileges = true;
+          PrivateTmp = true;
+          ProtectSystem = "strict";
+          ProtectHome = true;
+          Restart = "on-failure";
+          RestartSec = 5;
+          DynamicUser = true;
+        };
       };
     };
-
-    # TODO: Revisit when nixpkgs merges token-based tunnel support
-    # https://github.com/NixOS/nixpkgs/pull/427964
-    # At that point services.cloudflared.tunnels may replace this custom service.
-    systemd.services.cloudflared = {
-      wantedBy = [ "multi-user.target" ];
-      after = [
-        "network-online.target"
-        "opnix-secrets.service"
-      ];
-      wants = [ "network-online.target" ];
-      requires = [ "opnix-secrets.service" ];
-      serviceConfig = {
-        ExecStart = "${cloudflaredStartScript}";
-        LoadCredential = "token:${config.services.onepassword-secrets.secretPaths.cloudflareTunnelTokenFile}";
-        NoNewPrivileges = true;
-        PrivateTmp = true;
-        ProtectSystem = "strict";
-        ProtectHome = true;
-        Restart = "on-failure";
-        RestartSec = 5;
-        DynamicUser = true;
-      };
-    };
-  };
 }
