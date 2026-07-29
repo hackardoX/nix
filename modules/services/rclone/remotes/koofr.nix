46f177afc0ecd03c2c0af467113e1cd653cafc19
aaccardo <hackardo@gmail.com>
2026-07-29 19:59:37 +0200

    fix: revert rclone requireUnit
    Signed-off-by: aaccardo <hackardo@gmail.com>

--- modules/services/rclone/remotes/koofr.nix
+++ modules/services/rclone/remotes/koofr.nix
@@ -14,19 +14,16 @@ in
   flake.modules.darwin.rclone = polyModule;
 
   flake.modules.homeManager.rclone = hmArgs: {
-    programs.rclone = {
-      requiresUnit = "opnix-secrets.service";
-      remotes = lib.mkIf (builtins.elem "koofr" hmArgs.config.services.rclone.remotes) {
-        koofr = {
-          config = {
-            type = "koofr";
-            endpoint = "https://app.koofr.net";
-            user = config.flake.meta.users.aaccardo.email;
-          };
+    programs.rclone.remotes = lib.mkIf (builtins.elem "koofr" hmArgs.config.services.rclone.remotes) {
+      koofr = {
+        config = {
+          type = "koofr";
+          endpoint = "https://app.koofr.net";
+          user = config.flake.meta.users.aaccardo.email;
+        };
 
-          secrets = {
-            password = "/run/secrets/koofr/password";
-          };
+        secrets = {
+          password = "/run/secrets/koofr/password";
         };
       };
     };
