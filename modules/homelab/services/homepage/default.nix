5e3812b11d39db89610fd3e37f8b1d637f4eea86
aaccardo <hackardo@gmail.com>
2026-07-30 14:52:45 +0200

    fix: attempt fixing homepage
    Signed-off-by: aaccardo <hackardo@gmail.com>

--- modules/homelab/services/homepage/default.nix
+++ modules/homelab/services/homepage/default.nix
@@ -111,14 +111,9 @@ in
       home.username = homepageUser;
       home.stateVersion = "26.05";
       imports = with config.flake.modules.homeManager; [
-        homelab-docker-socket-proxy
         homelab-homepage
         homelab-podman-extension
       ];
-      services.homelab-docker-socket-proxy = {
-        enable = true;
-        port = config.flake.meta.reverse-proxy.ports.homepage-docker-socket-proxy;
-      };
     };
 
     systemd.tmpfiles.rules = [
