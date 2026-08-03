e71fc8c2e3e95299b131f45340c9a68c8741db1e
aaccardo <hackardo@gmail.com>
2026-08-03 13:12:25 +0200

    fix: attempt showing real ip caddy logs
    Signed-off-by: aaccardo <hackardo@gmail.com>

--- modules/homelab/reverse-proxy.nix
+++ modules/homelab/reverse-proxy.nix
@@ -103,7 +103,7 @@ in
               output file /var/lib/caddy/access.log {
                 roll_disabled
               }
-              format transform `{request>remote_ip} {request>host} - - [{ts}] "{request>method} {request>uri} {request>proto}" {status} {size}` {
+              format transform `{request>client_ip} {request>host} - - [{ts}] "{request>method} {request>uri} {request>proto}" {status} {size}` {
                 time_format "02/Jan/2006:15:04:05 -0700"
               }
             }
