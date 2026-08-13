aacf6ab283f8bba81e32f25326ca1c2acde34a2a
Andrea Accardo <hackardo@gmail.com>
2026-08-14 00:18:03 +0200

    feat: use redis for authelia sessions (#299)
    Signed-off-by: aaccardo <hackardo@gmail.com>

---
+++ modules/homelab/security/authelia/persisted-session.nix
@@ -0,0 +1,35 @@
+{ config, ... }:
+{
+  flake.modules.nixos.homelab-security = nixosArgs: {
+    services = {
+      authelia = {
+        instances.default = {
+          settings.session.redis.host = "/run/redis-authelia/redis.sock";
+          secrets.sessionSecretFile =
+            nixosArgs.config.services.onepassword-secrets.secretPaths.autheliaSessionSecret;
+        };
+      };
+
+      timesyncd.enable = false;
+      chrony.enable = true;
+
+      redis.servers.authelia = {
+        enable = true;
+        port = 0;
+      };
+    };
+
+    users.users.${config.flake.meta.users.authelia.name}.extraGroups = [
+      "redis-authelia"
+    ];
+
+    boot.initrd.impermanence.persist.files = [
+      "/var/lib/redis-authelia/dump.rdb"
+    ];
+
+    systemd.services.redis-authelia = {
+      before = [ "authelia-default.service" ];
+      wantedBy = [ "authelia-default.service" ];
+    };
+  };
+}
