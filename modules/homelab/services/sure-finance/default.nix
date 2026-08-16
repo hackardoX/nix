c767916d15cb6419b73208218a505dc40d3dc0af
Andrea Accardo <hackardo@gmail.com>
2026-08-16 16:39:49 +0200

    fix: sure finance configuration (#304)
    * fix: sure finance ai URI
    
    Signed-off-by: aaccardo <hackardo@gmail.com>
    
    * fix: update sure finance version
    
    Signed-off-by: aaccardo <hackardo@gmail.com>
    
    * fix: authelia redis permission folder
    
    Signed-off-by: aaccardo <hackardo@gmail.com>
    
    ---------
    
    Signed-off-by: aaccardo <hackardo@gmail.com>

--- modules/homelab/services/sure-finance/default.nix
+++ modules/homelab/services/sure-finance/default.nix
@@ -12,7 +12,7 @@ let
 
   hosts = config.flake.meta.reverse-proxy.hosts;
   reverseProxyPort = config.flake.meta.reverse-proxy.ports.sure-finance;
-  sureFinanceImage = "ghcr.io/we-promise/sure:0.7.2";
+  sureFinanceImage = "ghcr.io/we-promise/sure:0.7.3";
   sureFinancePort = 3000;
   sureFinanceDbName = "sure_production";
   sureFinanceDbUser = "sure_user";
@@ -210,7 +210,7 @@ in
         OIDC_REDIRECT_URI = "https://${hosts.finance}/auth/openid_connect/callback";
         OIDC_BUTTON_LABEL = "Sign in with Authelia";
         OPENAI_MODEL = "mimo-v2.5-free";
-        OPENAI_URI_BASE = "https://opencode.ai/zen/v1/chat/completions";
+        OPENAI_URI_BASE = "https://opencode.ai/zen/v1";
         ONBOARDING_STATE = "closed";
         POSTGRES_USER = sureFinanceDbUser;
         POSTGRES_DB = sureFinanceDbName;
