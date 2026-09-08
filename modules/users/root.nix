a9e332e838ebcdb587d3bce4eccefce8d303b54b
Andrea Accardo <andrea.accardo@proton.ch>
2026-09-08 11:20:17 +0200

    feat: store root hashed password to sops secrets
    Signed-off-by: Andrea Accardo <andrea.accardo@proton.ch>

--- modules/users/root.nix
+++ modules/users/root.nix
@@ -7,11 +7,15 @@
     uid = 0;
   };
 
-  flake.modules.nixos.root = {
+  flake.modules.nixos.root = nixosArgs: {
+    sops.secrets."root/hashed_password" = {
+      neededForUsers = true;
+    };
+
     users.users.${config.flake.meta.users.root.name} = {
       inherit (config.flake.meta.users.root) description uid;
       isNormalUser = false;
-      hashedPassword = "$6$bmMY3k2VAqr5sg2I$C14qsnZx7xxTs.XxTyL7/hYKsq4cjEfzibmEjqQQMc/2.fmt.N6qqhtGa1ckqpdqdnzurboSeZ/F/F4DuzySM/";
+      hashedPasswordFile = nixosArgs.config.sops.secrets."root/hashed_password".path;
     };
   };
 }
