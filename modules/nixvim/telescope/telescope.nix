01641b59fe8c268227340e951a713a583e6e1099
Andrea Accardo <andrea.accardo@proton.ch>
2026-09-11 14:42:52 +0200

    feat: update telescope layout
    Signed-off-by: Andrea Accardo <andrea.accardo@proton.ch>

--- modules/nixvim/telescope/telescope.nix
+++ modules/nixvim/telescope/telescope.nix
@@ -1,6 +1,13 @@
 {
   flake.modules.nixvim.dev.plugins = {
-    telescope.enable = true;
+    telescope = {
+      enable = true;
+      settings.defaults = {
+        layout_config = {
+          preview_width = 0.7;
+        };
+      };
+    };
     web-devicons.enable = true;
   };
 }
